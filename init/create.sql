e2d11aeb70bdcce93b44bfabee8168a26b47f0e1
mehdi <mehdi.munim@gmail.com>
2021-05-18 22:39:26 +0200

    Update create livres

--- init/create.sql
+++ init/create.sql
@@ -1,1 +1,21 @@
 /* Script pour créer toutes les tables */
+/* 
+Script pour créer la table livres
+*/
+
+CREATE TABLE livres (
+    Id INTEGER PRIMARY KEY,
+    Titre TEXT NOT NULL,
+    "Titre original" TEXT,
+    ISBN13 NUMERIC NOT NULL,
+    Genre TEXT NOT NULL,
+    "Maison d'édition" TEXT,
+    Auteur TEXT NOT NULL,
+    Date INTEGER,
+    "Date 1er publication" INTEGER,
+    Support TEXT NOT NULL,
+    Langue TEXT NOT NULL,
+    "Langue d'origine" TEXT NOT NULL,
+    Pays TEXT NOT NULL,
+    Traducteur TEXT
+);
--- init/fill.sql
+++ init/fill.sql
@@ -1,1 +1,8 @@
 /* Script pour remplir les tables grâce au CSV*/
+
+/* Remplit la table livres*/
+
+COPY livres
+FROM '/home/mehdi/E-commerce-DB-1/CSV/livres.csv'
+DELIMITER ',' CSV HEADER;
+
