acf5dd793d6a0bb24e53b93d806d5343ff3796f7
mehdi <mehdi.munim@gmail.com>
2021-05-18 21:24:24 +0200

    Update commandes_ret

--- CSV_creator/contenu_commandes_generator.py
+++ CSV_creator/contenu_commandes_generator.py
@@ -2,6 +2,7 @@ import csv
 import context
 from common import rand_price
 from common import get_column
+import random
 
 
 def contenu_commandes_generator():
@@ -15,29 +16,33 @@ def contenu_commandes_generator():
     # Id Prod is FK from Livres UNION Périodiques
     ids_commande = get_column("ID", "CSV/commandes.csv")
 
-    ids_client = [i+1 for i in range(1000)]
+    # clients between 1 and 1000 (see clients.csv)
+    ids_client = get_column("ID", "CSV/commandes.csv")
 
+    # dates des commandes correspondantes
+    dates_commande = get_column("Date de commande", "CSV/commandes.csv")
+
+    commandes_FK = [(ids_commande[i], ids_client[i], dates_commande[i])
+                    for i, _ in enumerate(ids_client)]
+
+    # les produits
     ids_prod = get_column("ID", "CSV/livres.csv")
     #ids_prod += get_column("SICI", "CSV/periodiques.csv")
 
-    # Limited set of dates
-    dates = ["17/05/2021", "18/05/2021",
-             "19/05/2021", "20/05/2021", "21/05/2021"]
-
-    etat = ["disponible", "livré"]
+    etats = ["disponible", "livré"]
     with open("CSV/contenu_commandes.csv", "w", encoding="UTF8") as f:
         writer = csv.writer(f)
 
         writer.writerow(header)
 
-        # 5 entries by product
-        for i in range(5):
-            for id_prod in ids_prod:
-
-                # random date
-                date = dates[i]
-
-                row = [id_prod, date, rand_price()]
+        # récupère les trois clés étrangères de commandes
+        for (id_commande, id_client, date_commande) in commandes_FK:
+            random.shuffle(ids_prod.tomutable())
+            for i in range(5):
+                etat = etats[i % 2]
+                id_prod = ids_prod[i]
+                row = [id_commande, id_client,
+                       date_commande, id_prod, etat, i]
                 writer.writerow(row)
 
 
