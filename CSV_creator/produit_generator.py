5a19f67611464dfc2a73f973f7bdb3df1c64ee5d
mehdi <mehdi.munim@gmail.com>
2021-05-23 12:54:31 +0200

    Update produit.csv

--- CSV_creator/produit_generator.py
+++ CSV_creator/produit_generator.py
@@ -1,4 +1,4 @@
-from .common import get_column
+from common import get_column
 import csv
 
 
@@ -10,19 +10,23 @@ def produit_generator():
     header = ["Id_prod", "Type"]
 
     # Id Prod is FK from Livres UNION Périodiques
-    ids_prod_livre = get_column("ID", "CSV/livre.csv")
-    ids_prod_periodique = get_column("ID", "CSV/periodique.csv")
+    ids_prod_livre = get_column("Id", "CSV/livre.csv")
+    ids_prod_periodique = get_column("Id", "CSV/numero_periodique.csv")
 
     with open("CSV/produit.csv", "w", encoding="UTF8") as f:
         writer = csv.writer(f)
 
         writer.writerow(header)
 
+        last_id_livre = 0
         for id_prod in ids_prod_livre:
             row = [id_prod, "livre"]
             writer.writerow(row)
+            last_id_livre = id_prod
+
         for id_prod in ids_prod_periodique:
-            row = [id_prod, "periodique"]
+            id_periodique = int(id_prod) + int(last_id_livre)
+            row = [str(id_periodique), "periodique"]
             writer.writerow(row)
 
 
