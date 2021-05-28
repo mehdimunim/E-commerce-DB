9ab41a319b604e2b6626ae72a62ac84c44b07769
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-05-28 12:58:56 +0200

    Update

--- init/fill.sql
+++ init/fill.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* Script pour remplir les tables grâce au CSV*/
 
 /* Remplit la table livres*/
@@ -6,3 +7,22 @@ COPY livre
 FROM '/home/mehdi/E-commerce-DB-1/CSV/livres.csv'
 DELIMITER ',' CSV HEADER;
 
+=======
+\! echo "\nREMPLISSAGE AVEC LES CSV\n"
+
+\COPY client from 'CSV/livre.csv' WITH (FORMAT CSV, HEADER);
+\COPY commande_annulee from 'CSV/commande_annulee.csv' WITH (FORMAT CSV, HEADER);
+\COPY commande from 'CSV/commande.csv' WITH (FORMAT CSV, HEADER);
+\COPY historique_des_prix from 'CSV/historique_des_prix.csv' WITH (FORMAT CSV, HEADER);
+\COPY livre from 'CSV/livre.csv' WITH (FORMAT CSV, HEADER);
+\COPY notation from 'CSV/notation.csv' WITH (FORMAT CSV, HEADER);
+\COPY numero_periodique from 'CSV/numero_periodique.csv' WITH (FORMAT CSV, HEADER);
+\COPY panier from 'CSV/panier.csv' WITH (FORMAT CSV, HEADER);
+\COPY periodique from 'CSV/periodique.csv' WITH (FORMAT CSV, HEADER);
+\COPY produit_commande from 'CSV/produit_commande.csv' WITH (FORMAT CSV, HEADER);
+\COPY produit_dans_panier from 'CSV/produit_dans_panier.csv' WITH (FORMAT CSV, HEADER);
+\COPY produit_effectif from 'CSV/produit_effectif.csv' WITH (FORMAT CSV, HEADER);
+\COPY produit_livre from 'CSV/produit_livre.csv' WITH (FORMAT CSV, HEADER);
+\COPY produit_retourne from 'CSV/produit_retourne.csv' WITH (FORMAT CSV, HEADER);
+\COPY produit from 'CSV/produit.csv' WITH (FORMAT CSV, HEADER);
+>>>>>>> parent of 52f2825 (Merge branch 'main' into Mehdi)
