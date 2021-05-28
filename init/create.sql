148ee0fc600fc91dc2e1d98391a671cd5b6cfb91
mehdimunim <68103264+mehdimunim@users.noreply.github.com>
2021-05-28 13:10:55 +0200

    Update

--- init/create.sql
+++ init/create.sql
@@ -12,18 +12,19 @@ DROP TABLE IF EXISTS commande CASCADE;
 DROP TABLE IF EXISTS commande_annulee CASCADE;
 DROP TABLE IF EXISTS produit_commande CASCADE;
 DROP TABLE IF EXISTS produit_livre CASCADE;
-DROP TABLE IF EXISTS historique_prix CASCADE;
+DROP TABLE IF EXISTS historique_des_prix CASCADE;
 DROP TABLE IF EXISTS produit_retourne CASCADE;
 DROP TABLE IF EXISTS notation CASCADE;
 
+\! echo "\nCREATION DES TABLES\n"
 
 CREATE TABLE client(
     id_client INTEGER PRIMARY KEY,
-    mail TEXT NOT NULL,
     prenom TEXT NOT NULL,
     nom TEXT NOT NULL,
     adresse TEXT NOT NULL,
-    telephone NUMERIC NOT NULL
+    mail TEXT NOT NULL,
+    telephone VARCHAR(20) NOT NULL
 ); 
 
 CREATE TABLE produit(
@@ -46,11 +47,12 @@ CREATE TABLE livre (
     langue TEXT NOT NULL,
     langue_origine TEXT NOT NULL,
     pays TEXT NOT NULL,
-    traducteur TEXT
+    traducteur TEXT,
+    FOREIGN KEY(id_livre) REFERENCES produit(id_produit)
 );
 
 CREATE TABLE periodique(
-    issn NUMERIC PRIMARY KEY,
+    issn VARCHAR(10) PRIMARY KEY,
     titre TEXT NOT NULL,
     periodicite VARCHAR(50),
     langue TEXT,
@@ -62,10 +64,11 @@ CREATE TABLE periodique(
 
 CREATE TABLE numero_periodique(
     id INTEGER PRIMARY KEY,
-    issn INTEGER REFERENCES periodique(issn),
+    issn VARCHAR(10) REFERENCES periodique(issn),
     num INTEGER,
     date_publication TEXT,
-    CHECK (num >= 1)
+    CHECK (num >= 1),
+    FOREIGN KEY(id) REFERENCES produit(id_produit)
 );
 
 CREATE TABLE produit_effectif(
@@ -73,7 +76,8 @@ CREATE TABLE produit_effectif(
     disponibilite VARCHAR(4),
     delai INTEGER,
     num INTEGER,
-    CHECK (num >= 0)
+    CHECK (num >= 0),
+    FOREIGN KEY(id) REFERENCES produit(id_produit)
 );
 
 CREATE TABLE panier(
@@ -82,63 +86,70 @@ CREATE TABLE panier(
 );
 
 CREATE TABLE produit_dans_panier(
-    id_prod INTEGER PRIMARY KEY,
-    id_panier INTEGER,
+    id_prod INTEGER REFERENCES produit(id_produit),
+    id_panier INTEGER REFERENCES panier(id_panier),
     quantite INTEGER,
+    PRIMARY KEY (id_prod, id_panier),
     CHECK (quantite >=0)
 );
 
 CREATE TABLE commande( 
     id_commande INTEGER PRIMARY KEY,
-    date_commande INTEGER,
-    id_panier INTEGER,
-    id_client INTEGER,
+    date_commande VARCHAR(10),
+    id_panier INTEGER REFERENCES panier(id_panier),
+    id_client INTEGER REFERENCES client(id_client),
     adresse_livraison TEXT NOT NULL,
-    prix_commande INTEGER,
+    prix_commande NUMERIC,
     mode_payement TEXT,
     effectivement_paye VARCHAR(3)
 );
 
 CREATE TABLE commande_annulee(
     id_commande INTEGER PRIMARY KEY,
-    date_annulation INTEGER
+    date_annulation VARCHAR(10),
+    FOREIGN KEY(id_commande) REFERENCES commande(id_commande)
 );
 
 CREATE TABLE produit_commande(
-    id_produit INTEGER PRIMARY KEY,
-    id_commande INTEGER,
-    quantite INTEGER
+    id_commande INTEGER REFERENCES produit(id_produit),
+    id_produit INTEGER REFERENCES commande(id_commande),
+    quantite INTEGER,
+    PRIMARY KEY (id_produit, id_commande)
 );
 
 CREATE TABLE produit_livre(
-    id_produit INTEGER PRIMARY KEY,
-    id_commande INTEGER,
-    date_livraison_effective INTEGER,
-    quantite_livree INTEGER
-
+    id_produit INTEGER REFERENCES produit(id_produit),
+    id_commande INTEGER REFERENCES commande(id_commande),
+    date_livraison_effective VARCHAR(10),
+    quantite_livree INTEGER,
+    PRIMARY KEY (id_produit, id_commande, date_livraison_effective)
 );
-
+/*Peut-on mettre une FK avec produit commande ou commande(date commande) ?*/
 CREATE TABLE produit_retourne( 
-    id_client INTEGER PRIMARY KEY,
-    id_produit INTEGER,
-    date_commande INTEGER,
+    id_client INTEGER REFERENCES client(id_client),
+    id_produit INTEGER REFERENCES produit(id_produit),
+    date_commande VARCHAR(10),
     motif TEXT,
-    montant NUMERIC
+    montant NUMERIC,
+    PRIMARY KEY (id_client, id_produit)
 );
 
-CREATE TABLE historique_prix(
-    id_prod INTEGER PRIMARY KEY REFERENCES produit(id_produit),
-    date_prix INTEGER NOT NULL,
-    prix_unitaire NUMERIC NOT NULL
+CREATE TABLE historique_des_prix(
+    id_prod INTEGER REFERENCES produit(id_produit),
+    date_prix VARCHAR(10),
+    prix_unitaire NUMERIC NOT NULL,
+    PRIMARY KEY (id_prod, date_prix)
 );
+
+/* Modifier les contraintes FK en accord avec la modélisation*/
 
 CREATE TABLE notation(
-    id_produit INTEGER PRIMARY KEY,
-    id_client INTEGER,
+    id_produit INTEGER REFERENCES produit(id_produit),
+    type_produit VARCHAR(20),
+    id_client INTEGER REFERENCES client(id_client),
     note INTEGER,
-    avis VARCHAR(50),
-    FOREIGN KEY(id_produit) REFERENCES produit(id_produit),
-    FOREIGN KEY(id_client) REFERENCES client(id_client),
+    avis TEXT,
+    PRIMARY KEY (id_produit, id_client, note),
     CHECK (note BETWEEN 1 AND 10)
 );
 
