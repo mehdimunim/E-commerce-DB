085f2d9920981a4fb515f6a5a948a1c9c0ba353e
mehdi <mehdi.munim@gmail.com>
2021-05-28 13:12:14 +0200

    Update

--- init/create.sql
+++ init/create.sql
@@ -141,21 +141,3 @@ CREATE TABLE notation(
     FOREIGN KEY(id_client) REFERENCES client(id_client),
     CHECK (note BETWEEN 1 AND 10)
 );
-
-\! echo "\nREMPLISSAGE AVEC LES CSV\n"
-
-\COPY client from 'CSV/client.csv' WITH (FORMAT CSV, HEADER);
-\COPY produit from 'CSV/produit.csv' WITH (FORMAT CSV, HEADER);
-\COPY livre from 'CSV/livre.csv' WITH (FORMAT CSV, HEADER);
-\COPY periodique from 'CSV/periodique.csv' WITH (FORMAT CSV, HEADER);
-\COPY numero_periodique from 'CSV/numero_periodique.csv' WITH (FORMAT CSV, HEADER);
-\COPY historique_des_prix from 'CSV/historique_des_prix.csv' WITH (FORMAT CSV, HEADER);
-\COPY panier from 'CSV/panier.csv' WITH (FORMAT CSV, HEADER);
-\COPY commande from 'CSV/commande.csv' WITH (FORMAT CSV, HEADER);
-\COPY produit_commande from 'CSV/produit_commande.csv' WITH (FORMAT CSV, HEADER);
-\COPY produit_dans_panier from 'CSV/produit_dans_panier.csv' WITH (FORMAT CSV, HEADER);
-\COPY produit_effectif from 'CSV/produit_effectif.csv' WITH (FORMAT CSV, HEADER);
-\COPY produit_livre from 'CSV/produit_livre.csv' WITH (FORMAT CSV, HEADER);
-\COPY produit_retourne from 'CSV/produit_retourne.csv' WITH (FORMAT CSV, HEADER);
-\COPY commande_annulee from 'CSV/commande_annulee.csv' WITH (FORMAT CSV, HEADER);
-\COPY notation from 'CSV/notation.csv' WITH (FORMAT CSV, HEADER);
