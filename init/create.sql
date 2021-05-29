862d3225fdb2faeb355bf79b8e936cea6edaef4e
Lauréna Porcel <lunaelfyr@yahoo.fr>
2021-05-29 13:15:21 +0200

    add date_naissance and date_inscription in client table

--- init/create.sql
+++ init/create.sql
@@ -15,6 +15,7 @@ DROP TABLE IF EXISTS produit_livre CASCADE;
 DROP TABLE IF EXISTS historique_des_prix CASCADE;
 DROP TABLE IF EXISTS produit_retourne CASCADE;
 DROP TABLE IF EXISTS notation CASCADE;
+DROP TYPE IF EXISTS etat_commande;
 
 \! echo "\nCREATION DES TABLES\n"
 
@@ -25,11 +26,14 @@ CREATE TABLE client(
     adresse TEXT NOT NULL,
     mail TEXT NOT NULL,
     telephone VARCHAR(20) NOT NULL
+	date_naissance DATE,
+    date_inscription DATE
 ); 
 
 CREATE TABLE produit(
     id_produit INTEGER PRIMARY KEY,
-    type_produit TEXT
+    type_produit TEXT,
+	delai_retour INTEGER
 );
 
 
@@ -93,26 +97,27 @@ CREATE TABLE produit_dans_panier(
     CHECK (quantite >=0)
 );
 
+CREATE TYPE etat_commande AS ENUM ('en_attente','en_preparation','livree','annulee');
 CREATE TABLE commande( 
     id_commande INTEGER PRIMARY KEY,
     date_commande VARCHAR(10),
-    id_panier INTEGER REFERENCES panier(id_panier),
-    id_client INTEGER REFERENCES client(id_client),
     adresse_livraison TEXT NOT NULL,
     prix_commande NUMERIC,
     mode_payement TEXT,
-    effectivement_paye VARCHAR(3)
-);
+    effectivement_paye BOOLEAN,
+	etat etat_commande
+)INHERITS(panier);
 
 CREATE TABLE commande_annulee(
     id_commande INTEGER PRIMARY KEY,
     date_annulation VARCHAR(10),
+	remboursement BOOLEAN,
     FOREIGN KEY(id_commande) REFERENCES commande(id_commande)
 );
 
 CREATE TABLE produit_commande(
-    id_commande INTEGER REFERENCES produit(id_produit),
-    id_produit INTEGER REFERENCES commande(id_commande),
+    id_commande INTEGER REFERENCES commande(id_commande),
+    id_produit INTEGER REFERENCES produit(id_produit),
     quantite INTEGER,
     PRIMARY KEY (id_produit, id_commande)
 );
