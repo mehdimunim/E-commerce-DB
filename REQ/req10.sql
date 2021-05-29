80f0946215fae92a1e4920331ecfcb91bc04e827
mehdi <mehdi.munim@gmail.com>
2021-05-29 19:18:32 +0200

    Update req9 and 10

--- REQ/req10.sql
+++ REQ/req10.sql
@@ -1,2 +1,31 @@
-/* Requête avec condition de totalité (agrégation)*/
-/* Les commandes qui contiennent un maximum de livres*/
+/* Requête avec condition de totalité (sous requête corrélée)*/
+
+/* Les clients qui ont acheté tous les livres de Stendhal disponibles*/
+
+SELECT id_client
+FROM (produit_commande pc
+JOIN commande c
+ON  pc.id_commande = c.id_commande
+JOIN livre l
+ON 
+pc.id_produit = l.id_livre) J
+WHERE
+-- Il n'y a pas de livre de Stendhal qui ne se trouve pas dans les produits commandés du client
+NOT EXISTS 
+-- livres écrits par Stendhal
+(SELECT DISTINCT id_produit
+FROM livre
+WHERE auteur = 'Stendhal'
+AND id_produit
+NOT IN
+-- produits commandés par le client
+(
+SELECT id_produit 
+FROM (produit_commande pc1
+JOIN commande c1
+ON  pc1.id_commande = c1.id_commande) J1
+WHERE J.id_client = J1.id_client
+)
+
+)
+;
--- REQ/req9.sql
+++ REQ/req9.sql
@@ -1,6 +1,24 @@
-/* Requête avec condition de totalité (sous requête corrélée)*/
+/* Requête avec condition de totalité (agrégation)*/
 
-/* Les commandes qui contiennent un maximum de livres*/
+/* Les clients qui ont acheté tous les livres de Stendhal disponibles*/
+
+SELECT id_client
+FROM produit_commande pc
+JOIN commande c
+ON  pc.id_commande = c.id_commande
+JOIN livre l
+ON 
+pc.id_produit = l.id_livre
+WHERE auteur = 'Stendhal'
+GROUP BY id_client 
+HAVING COUNT(DISTINCT titre)
+= 
+(SELECT COUNT(DISTINCT titre)
+FROM livre
+WHERE auteur = 'Stendhal'
+)
+;
+
 
 
 
