f1b630e84134203beea34addcb94311f50aa9505
Lauréna Porcel <lunaelfyr@yahoo.fr>
2021-05-30 19:38:44 +0200

    add new requests

--- REQ/req15.sql
+++ REQ/req15.sql
@@ -1,1 +1,10 @@
-/* Requête avec des NULL */
+/* Requête portant sur 3 tables */
+
+-- Liste des numéros des periodiques hebdomadaires :
+
+SELECT periodique.issn, periodique.titre, numero_periodique.date_publication
+	FROM numero_periodique, periodique, produit
+	WHERE numero_periodique.issn=periodique.issn
+		AND produit.id_produit=numero_periodique.id
+		AND type_produit='periodique'
+		AND periodicite='Hebdomadaire';
---
+++ REQ/req19.sql
@@ -0,0 +1,7 @@
+/* Jointure externe */
+
+-- Envoyer un email de PUB aux clients qui n'ont jamais fait de commande :
+
+SELECT client.id_client, nom, prenom, mail FROM client 
+    LEFT JOIN commande ON client.id_client = commande.id_client 
+    WHERE commande.id_client is NULL;
