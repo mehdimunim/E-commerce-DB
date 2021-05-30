88083726285c889c44c3c3b741914e558f81d238
Md017 <79168793+Md017@users.noreply.github.com>
2021-05-30 20:54:19 +0200

    update req13

--- REQ/req13.sql
+++ REQ/req13.sql
@@ -2,4 +2,4 @@
  SELECT produit.id_produit,produit.type_produit,AVG(prix_commande) AS "prix moyen"
  FROM commande NATURAL JOIN produit_commande NATURAL JOIN produit
  GROUP BY produit.type_produit,produit.id_produit
- ORDER BY produit.type_produit;
+ ORDER BY "prix moyen";
--- REQ/req17.sql
+++ REQ/req17.sql
@@ -2,8 +2,8 @@
 
 \! echo "\nLes clients ayant apprécié que les livres\n"
  SELECT prenom,nom,client.id_client,id_produit,type_produit,note,avis
- FROM notations NATURAL JOIN client
+ FROM notation NATURAL JOIN client
  WHERE id_client IN
             (SELECT id_client
-             FROM notations
+             FROM notation
              WHERE (type_produit = 'livre'AND note>=5));
