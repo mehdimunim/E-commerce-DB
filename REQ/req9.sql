c790ab0458ad269747ffd4430b05029619fc5f6a
mehdi <mehdi.munim@gmail.com>
2021-05-30 13:25:28 +0200

    Update req9 and 10

---
+++ REQ/req9.sql
@@ -0,0 +1,20 @@
+/* Requête avec condition de totalité (agrégation)*/
+
+/* Les clients qui ont acheté tous les livres de Stendhal disponibles*/
+
+SELECT id_client
+FROM produit_commande pc
+JOIN commande c
+ON  pc.id_commande = c.id_commande
+JOIN livre l
+ON 
+pc.id_produit = l.id_livre
+WHERE auteur = 'Stendhal'
+GROUP BY id_client 
+HAVING COUNT(DISTINCT titre)
+= 
+(SELECT COUNT(DISTINCT titre)
+FROM livre
+WHERE auteur = 'Stendhal'
+)
+;
